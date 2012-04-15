f85250cc539e5555db273f81d237ac6010ea6401
Wang Haibo <angelo@Wangs-MacBook-Pro.local>
2012-04-15 08:19:27 +0800

    Initial Commit

---
+++ ObjectiveCExercise/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ObjectiveCExercise
+//
+//  Created by Haibo Wang on 4/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
