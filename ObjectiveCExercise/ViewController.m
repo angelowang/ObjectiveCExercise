0753ed1f2432202b2ac05c7ebc47708bd9f96e95
Wang Haibo <angelo@Wangs-MacBook-Pro.local>
2012-04-15 09:29:08 +0800

    Initial

--- ObjectiveCExercise/ViewController.m
+++ ObjectiveCExercise/ViewController.m
@@ -13,15 +13,24 @@
 @end
 
 @implementation ViewController
+@synthesize label;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    label.text = @"Exercise: \r\n"
+    "1. Write a Shape class, with a name property; and an initWithName: method;\r\n"
+    "2. Write a Drawable protocol, with a draw method which returns a NSString *;\r\n"
+    "3. Write Rectangle, Circle classes inheriting Shape, and adopting Drawable;\r\n"
+    "4. Draw method should return \"Rectangle XX\", and \"Circle YY\"\r\n\r\n"
+    "Bonus:\r\n"
+    "Prepare the model class for calculator brain.";
 }
 
 - (void)viewDidUnload
 {
+    [self setLabel:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -34,5 +43,9 @@
         return YES;
     }
 }
+
+- (IBAction)startTest:(id)sender {
+}
+
 
 @end
